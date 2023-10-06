d713609a1908264eed706e565c3098f605bf72ec
chubbyhubbybubby <austinc2026@cis.edu.hk>
2023-10-06 09:43:55 +0800

    nice

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
+
 public class Main {
 
-    // Question 1 - calculateGrade
+    public static char calculateGrade(int score) {
+        if(score >= 90) return 'A';
+        else if(score >= 80) return 'B';
+        else if(score >= 70) return 'C';
+        else if(score >= 60) return 'D';
+        else if(score >= 50) return 'E';
+        else return 'F';
+    }
 
-    // Question 2 - fizzBuzz
+    public static String fizzBuzz(int num) {
+        if(num % 3 == 0 && num % 5 == 0) return "fizzbuzz";
+        else if(num % 3 == 0) return "fizz";
+        else if(num % 5 == 0) return "buzz";
+        else return "unlucky";
+    }
 
-    // Question 3 - frontBack
+    public static String frontBack(String str) {
+        if(str.length() < 2) return str;
+        return str.charAt(0) + "" + str.charAt(1) + str + str.charAt(0)+""+ str.charAt(1);
+    }
 
-    // Question 4 - twoAsOne
+    public static boolean twoAsOne(int a, int b, int c) {
+        return a + b == c || a + c == b || b + c == a;
+    }
 
-    // Question 5 - endUp
+    public static String endUp(String str) {
+        if(str.length() < 3) return str.toUpperCase();
+        return str.substring(0, str.length()-3);
+    }
 
 }
